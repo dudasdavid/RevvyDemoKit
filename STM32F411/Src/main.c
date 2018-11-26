081dd18a989322900feea0e0e4642f5f0a2f3aad
Dániel Buga <buga.daniel@outlook.com>
2018-11-26 21:40:25 +0100

    Generate TrueSTUDIO project

--- STM32F411/Src/main.c
+++ STM32F411/Src/main.c
@@ -948,7 +948,7 @@ static void MX_TIM9_Init(void)
 
   /* USER CODE END TIM9_Init 1 */
   htim9.Instance = TIM9;
-  htim9.Init.Prescaler = 99;
+  htim9.Init.Prescaler = 0;
   htim9.Init.CounterMode = TIM_COUNTERMODE_UP;
   htim9.Init.Period = 2400;
   htim9.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
